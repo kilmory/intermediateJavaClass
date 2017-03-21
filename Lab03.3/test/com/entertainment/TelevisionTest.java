dab87427332d691cbc58069705abb8887463bb0f
Nick <Nick@NickDesktop>
2017-03-20 20:29:19 -0500

    homework

---
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -0,0 +1,34 @@
+package com.entertainment;
+
+public class TelevisionTest {
+	
+	@Before
+	public void setup() {
+
+	}
+
+	public void testSetVolume() {
+
+	}
+
+	public void testSetVolumeIllegalArgumentException() {
+
+	}
+
+	public void testChangeChannel() {
+
+	}
+
+	public void testCompareTo() {
+
+	}
+
+	public void testEquals() {
+
+	}
+
+	public void testHashCode() {
+
+	}
+
+}
