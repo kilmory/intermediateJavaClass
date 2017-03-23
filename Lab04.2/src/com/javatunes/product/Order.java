e5d5e91363558f5fde189ab0994e2d428fe21664
Nick <Nick@NickDesktop>
2017-03-22 20:14:07 -0500

    more labs

--- Lab04.2/src/com/javatunes/product/Order.java
+++ Lab04.2/src/com/javatunes/product/Order.java
@@ -11,22 +11,55 @@ package com.javatunes.product;
 import java.util.Collection;
 
 public class Order {
-  private String id;
-  
-  public Order(String id) {
-    this.id = id;
-  }
-  
-  /**
+	private String id;
+
+	public Order(String id) {
+		this.id = id;
+	}
+
+	/**
    * TODO:
    * get the items from the cart and iterate over them, print each item's product code
    * get cart total and print
    */
-  public void processCart(Object reference_to_cart) {
-    
+  public void processCart(ShoppingCart<? extends Product> cart) {
+	  System.out.println("Order: " + getId() + " contains the following:");
+	  Collection<? extends Product> cartItems = cart.allItems();
+	  
+	  for (Product p : cartItems) {
+		  System.out.println(p.getCode());
+		  
+	  }
+	  System.out.println("Order Total: " + cart.total());
+	  
   }
-  
-  public String getId() {
-    return id;
-  }
+
+	public void processAllCarts(ShoppingCart<? extends Product>... carts) {
+
+		double orderTotal = 0.0;
+		System.out.println("Order: " + getId() + " contains the following:");
+		// iterate over varargs of cart(s) - carts numbered for display (too
+		// fancy?)
+		int cartNumber = 1;
+		for (ShoppingCart<? extends Product> cart : carts) {
+			System.out.println("Shopping Cart " + cartNumber + ":");
+			// get the items from the cart and iterate over them, print each
+			// item's product code
+			// get cart total and print
+			// add cart total to order total
+			Collection<? extends Product> cartItems = cart.allItems();
+			for (Product product : cartItems) {
+				System.out.println(product.getCode());
+			}
+			double cartTotal = cart.total();
+			System.out.println("Cart total: " + cartTotal);
+			orderTotal += cartTotal;
+			cartNumber++;
+		}
+		System.out.println("ORDER TOTAL: " + orderTotal);
+	}
+
+	public String getId() {
+		return id;
+	}
 }
--- Lab04.2/test/com/javatunes/product/OrderTest.java
+++ Lab04.2/test/com/javatunes/product/OrderTest.java
@@ -23,9 +23,24 @@ public class OrderTest {
     mediaCart.addItem(new MediaPlayer("AAC-PL233"));
   }
   
-  // TODO: create new Order for each cart above and pass cart to processOrder()
+  // DONE: create new Order for each cart above and pass cart to processOrder()
   @Test
   public void testProcessCart() {
+    Order order1 = new Order("order-1");
+    order1.processCart(productCart);
     
+    Order order2 = new Order("order-2");
+    order2.processCart(musicCart);
+    
+    Order order3 = new Order("order-3");
+    order3.processCart(mediaCart);
+  }
+  
+  // OPTIONAL
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testProcessAllCarts() {
+    Order order = new Order("order-1");
+    order.processAllCarts(productCart, musicCart, mediaCart);
   }
 }
